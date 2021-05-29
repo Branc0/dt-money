fd40b7469fa0354aecc5b8df1e96004d375ef0d6
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2021-05-29 20:20:37 -0300

    feat: install styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import styled from 'styled-components';
 import React from 'react';
+
+const Title = styled.h1`
+  color: #8257E6;
+`
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Title>Hello</Title>
     </div>
   );
 }
