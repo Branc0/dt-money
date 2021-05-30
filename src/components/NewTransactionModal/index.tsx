5df19620db19337628da6a369960e4da4f774995
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2021-05-30 16:43:00 -0300

    feat: style modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,8 @@
 import Modal from 'react-modal';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
+import closeImg from '../../assets/close.svg';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -7,8 +11,39 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
     return (
-        <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modaloverlay"
+            className="react=modal-content"
+        >
+
+            <button
+                type="button"
+                onClick={onRequestClose}
+                className="react-modal-close">
+                <img src={closeImg} alt="fechar modal" />
+            </button>
+
             <h2>Cadastrar transação</h2>
+            <Container>
+                <input type="text" placeholder="Título" />
+                <input type="number" placeholder="Valor" />
+                <TransactionTypeContainer>
+                    <button>
+                        <img src={incomeImg} alt="entrada" />
+                        <span>Entrada</span>
+                    </button>
+                    <button>
+                        <span>Saída</span>
+                        <img src={outcomeImg} alt="saída" />
+                    </button>
+                </TransactionTypeContainer>
+                <input type="text" placeholder="Categoria" />
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
         </Modal>
 
     )
